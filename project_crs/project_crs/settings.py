610931f8d82a78783004d8339d4d91bb314533a8
BenjaminGautier01 <benjamingautier7@gmail.com>
2024-04-28 09:18:09 +0100

    update

--- project_crs/project_crs/settings.py
+++ project_crs/project_crs/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 import dj_database_url
 from decouple import config
 
@@ -143,4 +143,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+
