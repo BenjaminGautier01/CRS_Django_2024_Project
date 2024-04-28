27a6ac3c0592b113a4032550276eb93b3c92ecc8
BenjaminGautier01 <benjamingautier7@gmail.com>
2024-04-28 11:05:55 +0100

    update

--- project_crs/project_crs/settings.py
+++ project_crs/project_crs/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g@kw%w2cd6gb@3r7(0=!*(otuu)lrrbbw%@rab%3k+70p@gfx9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
